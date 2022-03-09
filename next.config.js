83336a6c1751fdb0a0417d34f12b28258617e8b0
Kirk Daniels <kirkd@ayoba.me>
2022-03-09 23:27:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const contentful = require("contentful");
 
 module.exports = {
   images: {
+    loader: "imgix",
+    path: "https://epokothweni.netlify.app",
     domains: ["images.ctfassets.net"],
   },
 };
