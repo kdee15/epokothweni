1633720c2ae05b0ebc2f52508b60d97570d92f10
Kirk Daniels <kirkd@ayoba.me>
2022-03-09 23:24:09 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const contentful = require("contentful");
 
 module.exports = {
   images: {
-    loader: "imgix",
-    path: "http://localhost:3000/",
     domains: ["images.ctfassets.net"],
   },
 };
