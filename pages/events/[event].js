d5def0a3c65867533f63a42dd4858a75cd995799
Kirk Daniels <kirkdaniels@me.com>
2022-12-05 22:46:24 +0200

    events pages built

---
+++ pages/events/[event].js
@@ -0,0 +1,83 @@
+import classes from "./events.module.scss";
+
+const {
+  C_DELIVERY_KEY,
+  C_GRAPHQL_URL,
+} = require("../../helpers/contentful-config");
+const { EVENT_CONTENT, EVENT_SLUG } = require("../../helpers/data/events");
+
+/**
+ * Initial page load to access users browser information
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+export default function Event({ event }) {
+  console.log("event", event);
+  return (
+    <div className={classes.oProjectPage}>
+      <h1>Hello {event.title}</h1>
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const { event } = params;
+
+  const result = await fetch(C_GRAPHQL_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${C_DELIVERY_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: EVENT_CONTENT,
+      variables: {
+        slug: event,
+      },
+    }),
+  });
+
+  if (!result.ok) {
+    console.error(result);
+    return {};
+  }
+
+  const { data } = await result.json();
+  const [eventData] = data.eventCollection.items;
+
+  return {
+    props: { event: eventData },
+  };
+}
+
+export async function getStaticPaths() {
+  const result = await fetch(C_GRAPHQL_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${C_DELIVERY_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: EVENT_SLUG,
+    }),
+  });
+
+  if (!result.ok) {
+    return {};
+  }
+
+  const { data } = await result.json();
+  const eventSlug = data.eventCollection.items;
+  const paths = eventSlug.map(({ slug }) => {
+    return {
+      params: { event: slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
