1c19c72a6e4c092113c72f53cfc61e40ac8064d1
intembeko <kirkdaniels@me.com>
2024-03-18 23:00:51 +0200

    rework

--- src/components/blocks/componentHeroBanner/ComponentHeroBanner.js
+++ src/components/blocks/componentHeroBanner/ComponentHeroBanner.js
@@ -1,22 +1,39 @@
-import classes from "./ComponentHeroBanner.module.scss";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
-import React from "react";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import classes from "./ComponentHeroBanner.module.scss";
 
 function ComponentHeroBanner({ heroBanner }) {
+  const logoImage = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: true,
+        start: "top",
+        end: "+=500px",
+      },
+    });
+
+    timeline.to(logoImage.current, { height: "200px", opacity: "0" }, 0);
+  }, []);
+
   const { description, image, logo, theme } = heroBanner;
   return (
     <section
       className={`${classes.oHeroBlock} hero__${theme}`}
       style={{
-        backgroundImage: `url(http:${image.fields.file.url})`,
+        backgroundImage: `url(http:${image?.fields?.file?.url})`,
       }}
     >
+      <div className={`${classes.aBackgroundBlock}`}></div>
       <div className={`container`}>
         <div className={`${classes.oContentRow} row`}>
-          <div
-            className={`${classes.oContentBlock} col-12 col-md-6 offset-md-6 col-12 col-lg-7 offset-lg-5`}
-          >
+          <div className={`${classes.oContentBlock} col-12`}>
             <figure className={classes.mImage}>
               <Image
                 className={`${classes.aImage} a-responsive-image`}
@@ -27,11 +44,9 @@ function ComponentHeroBanner({ heroBanner }) {
                 aria-hidden="true"
                 style={{ objectFit: "contain" }}
                 priority="true"
+                ref={logoImage}
               />
             </figure>
-            <div className={`${classes.aText} a-fnt-18f`}>
-              {/* {documentToReactComponents(description)} */}
-            </div>
           </div>
         </div>
       </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,10 @@
 import { createClient } from "contentful";
 import Nav from "../components/molecules/nav/Nav";
 import ComponentHeroBanner from "../components/blocks/componentHeroBanner/ComponentHeroBanner";
-import ComponentServiceListing from "../components/blocks/componentServiceListing/ComponentServiceListing";
 import ComponentLatestPodcast from "../components/blocks/componentLatestPodcast/ComponentLatestPodcast";
 import ComponentCopy from "../components/organisms/componentCopy/ComponentCopy";
 import ComponentFeaturedNews from "../components/blocks/componentFeaturedNews/ComponentFeaturedNews";
-
 import ComponentFooter from "../components/blocks/componentFooter/ComponentFooter";
-import ComponentFeaturedEvents from "../components/blocks/componentFeaturedEvent/ComponentFeaturedEvent";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -51,20 +48,15 @@ export default function Home({ Page, News, Events, footer }) {
   const navLinks = Page[0].fields.components[0].fields;
   const heroBanner = Page[0].fields.components[1].fields;
   const ComponentAbout = Page[0].fields.components[2].fields;
-  const componentServiceListing = Page[0].fields.components[3].fields;
-  const componentLatestPodcast = Page[0].fields.components[4].fields;
+  const componentLatestPodcast = Page[0].fields.components[3].fields;
 
   return (
     <div>
       <Nav contentModule={navLinks} theme="home" />
       <ComponentHeroBanner heroBanner={heroBanner} />
       <ComponentCopy contentModule={ComponentAbout} />
-      <ComponentServiceListing contentModule={componentServiceListing} />
       <ComponentLatestPodcast contentModule={componentLatestPodcast} />
       {News.items.length && <ComponentFeaturedNews contentModule={News} />}
-      {Events.items.length && (
-        <ComponentFeaturedEvents contentModule={Events} />
-      )}
       <ComponentFooter footer={footer} />
     </div>
   );
